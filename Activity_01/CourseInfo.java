386ffd2f5903226693bf28a3d4f68335b77dec06
Emily Hollingshead <42753870+enhatl@users.noreply.github.com>
2022-05-22 13:02:01 -0500

    Add files via upload

---
+++ Activity_01/CourseInfo.java
@@ -0,0 +1,26 @@
+/** 
+ * Prints a description of the course. 
+ *
+ * Activity 1
+ * @author Emily Hollingshead - Comp 1210 - 005
+ * @version 01.18.22
+ */
+public class CourseInfo
+{
+   /**
+    * Prints course information to std output.
+    *
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args)
+   {
+     
+      // print course description
+      System.out.println("This course provides an introduction to Java and ");
+      System.out.println("object-oriented programming.");
+      System.out.println();
+      // print more course description
+      System.out.println("The course also introduces the basics"
+         + " of software development.");
+   }
+}
---
+++ Activity_01/StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+ * Simple program to print lines of text.
+ *
+ * Activity 01
+ * @author Emily Hollingshead - Comp 1210 - 005
+ * @version 01.18.22
+ */
+public class StudentInfo
+{
+   /**
+    * Prints information requested for the assignment.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Emily Hollingshead");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   Comp 1200");
+   }
+}
